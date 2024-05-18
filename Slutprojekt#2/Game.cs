36fef67c8048b2b9f9a3953dfcb7f910b4ecb917
Haddy Sosseh <Haddy.Sosseh@elev.ga.ntig.se>
2024-05-18 17:31:25 +0200

    commit 1

---
+++ Slutprojekt#2/Game.cs
@@ -0,0 +1,10 @@
+using System;
+
+public static class GameMethods{
+
+    private static string princessname;
+    private static int princessHP = 100;
+    private static int dragonHP = 100;
+
+
+}
